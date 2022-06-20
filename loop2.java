f206f80b0f42605b1d73f595c2355e97e21a2d01
Chandresh Verma <chandreshverma200@gmail.com>
2022-06-20 15:35:57 +0530

    changes to loop2

--- loop2.java
+++ loop2.java
@@ -2,21 +2,17 @@ import java.util.*;
 public class loop2 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int n=0;
-        for(int i=0;i<a;i++){
-            int b1 = scan.nextInt();
-            int b2 = scan.nextInt();
-            int b3 = scan.nextInt();
-            int b = b1+b2;
-            int c = b;
-            System.out.printf(""+b);
-            while(n<(b3-1)){
-                c = c + (2^n)*b2;
-                n=n+1;
-                System.out.printf(" "+c);
+        int x = scan.nextInt();
+        for(int i=0;i<x;i++){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int n = scan.nextInt();
+            int s = a;
+            for(int j=0;j<n;j++){
+                s += Math.pow(2,j)*b;
+                System.out.printf(""+s+" ");
             }
-            System.out.printf("%n");
+            System.out.println();
         }
         scan.close();
     }
